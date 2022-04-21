0e2b873e2d3d74167211c018ed5d63f150f3b5c5
IvanLecic97 <ika619@gmail.com>
2022-04-21 23:53:59 +0200

    Added register, login and logout

--- client-ui/src/App.js
+++ client-ui/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Route, Routes, BrowserRouter, Link } from "react-router-dom";
+import Register from "./components/register";
+import Login from "./components/login";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import axios from "axios";
+const App = () => {
+  const [usernameForDeletion, setUsernameForDeletion] = useState(
+    localStorage.getItem("username")
   );
-}
+  let onLogoutClick = (event) => {
+    const url =
+      "http://localhost:8080/WAR2022/rest/chat/users/loggedIn/" +
+      usernameForDeletion;
+    axios.delete(url).then((response) => {
+      console.log("Izlogovan!!!");
+    });
+
+    localStorage.removeItem("username");
+    console.log(localStorage.getItem("username"));
+  };
+  return (
+    <React.Fragment>
+      <Navbar bg="primary" variant="dark">
+        <Nav.Link
+          style={{ color: "white" }}
+          className="navLink"
+          href="/register"
+        >
+          Register
+        </Nav.Link>
+
+        <Nav.Link className="navLink" style={{ color: "white" }} href="/login">
+          Login
+        </Nav.Link>
+        <Nav.Link
+          onClick={onLogoutClick}
+          className="navLink"
+          style={{ color: "white" }}
+          href="/login"
+        >
+          Logout
+        </Nav.Link>
+      </Navbar>
+
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </React.Fragment>
+  );
+};
 
 export default App;
